66956cfd627fb7fca9fca4490cf2cdb3830a2999
jaskaran singh <90298857+jaskaranpr@users.noreply.github.com>
2022-03-06 14:05:46 +0530

    posts done

--- components/posts.js
+++ components/posts.js
@@ -3,9 +3,28 @@ const Posts = (element, data) => {
   data.map((post) => {
     let div = document.createElement("div");
     div.classList.add("postDiv");
-    let img = document.createElement("img");
-    img.src = post.cover.url;
-    div.append(img);
+    let content;
+    if (
+      post.cover.ext === "jpeg" ||
+      post.cover.ext === "png" ||
+      post.cover.ext === "jpg"
+    ) {
+      content = document.createElement("img");
+      content.src = post.cover.url;
+    } else {
+      content = document.createElement("video");
+      content.classList.add("postVideo");
+      // content.setAttribute("muted", "");
+      content.setAttribute("autoplay", "");
+      let source = document.createElement("source");
+      source.src = post.cover.url;
+      source.type = post.cover.mime_type;
+      content.append(source);
+    }
+    div.classList.add(
+      post.cover.width > post.cover.height ? "vertical" : "horizontal"
+    );
+    div.append(content);
     element.append(div);
   });
 };
