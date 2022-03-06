bdfcc31b73d638bfc200e7da320e9c449af3c931
jaskaran singh <90298857+jaskaranpr@users.noreply.github.com>
2022-03-06 13:08:59 +0530

    posts display

--- components/posts.js
+++ components/posts.js
@@ -1,2 +1,12 @@
-const Posts = (element, data) => {};
+const Posts = (element, data) => {
+  console.log(element, data);
+  data.map((post) => {
+    let div = document.createElement("div");
+    div.classList.add("postDiv");
+    let img = document.createElement("img");
+    img.src = post.cover.url;
+    div.append(img);
+    element.append(div);
+  });
+};
 export default Posts;
